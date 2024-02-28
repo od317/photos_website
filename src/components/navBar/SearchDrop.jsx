e6b89d68b7fdeb47ee0c9b6727c0aaf43eb88d7b
unknown <osamadrbas31@gmail.com>
2024-02-28 12:10:34 +0200

    making website faster

--- src/components/navBar/SearchDrop.jsx
+++ src/components/navBar/SearchDrop.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 function SearchDrop({prevSearch,handleLinkClick}) {
     const data = prevSearch.filter((v,i)=>v[0].length>0).slice(-6)
@@ -12,7 +13,11 @@ function SearchDrop({prevSearch,handleLinkClick}) {
                       {data.map((v,i)=>{
                             return(<button onClick={()=>handleLinkClick(v[0])} className='w-[100%] cursor-pointer group flex flex-row mt-[5%]  items-center' key={v+" "+i}>
                                     <div className=' w-full md:w-[40%] cursor-pointer bg-hov group-hover:bg-hov2 bg-cover bg-center relative pb-[42%] rounded-l-xl rounded-r-xl md:rounded-r-[0]'>
-                                        <img className=' absolute  h-full rounded-l-xl' src={v[1]} alt="" />
+                                        <LazyLoadImage
+                                          className='absolute  h-full rounded-l-xl'
+                                          alt={v[0]}
+                                          src={v[1]} 
+                                     />
                                     </div>
                                     <label className='p-[2%] hidden cursor-pointer bg-hov group-hover:bg-hov2 w-[60%] rounded-r-xl h-full md:flex items-center' htmlFor="">{v[0]}</label>
                             </button>)
