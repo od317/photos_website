2d6f0fad59b69ee7072fb7ddd3efffd56ed72910
unknown <osamadrbas31@gmail.com>
2024-02-28 17:17:28 +0200

    editing masonry grid

--- src/components/grid/Masonry.jsx
+++ src/components/grid/Masonry.jsx
@@ -24,8 +24,13 @@ function Masonry({data}) {
                         // ,height:`${(v.height*((((w*96/100)*per/100)/v.width)*100))/100}px`
                         <div key={v.id} style={{transform:`translateX(${arr[i][0]*100}%) translateY(${arr[i][1]}px)`}} 
                         className=' bg-cover p-[.5%] absolute transition-all duration-75 w-[50%] sm:w-[33%] md:w-[25%] lg:w-[20%] overflow-hidden bg-center'>
-                                  <div className='h-full' style={{backgroundColor:v.color}}>
+                                  <div className='h-full relative rounded-2xl' style={{backgroundColor:v.color}}>
+                                        <div className=' absolute text-white w-full h-full z-[3] bg-black opacity-[0%] bg-opacity-[0%] transition-all duration-200
+                                         hover:bg-opacity-[50%] hover:opacity-[100%] p-[2%] rounded-2xl'>
+                                            sss
+                                        </div>
                                         <LazyLoadImage
+                                            className='rounded-2xl z-[2]'
                                             alt={v.alt}
                                             src={v.urls.small} 
                                             />
