c1b8cfbabe4dd4e1707aa28d2024fd2e401cd32f
unknown <osamadrbas31@gmail.com>
2024-02-28 13:58:03 +0200

    editing small form

--- src/components/navBar/SearchDrop.jsx
+++ src/components/navBar/SearchDrop.jsx
@@ -12,14 +12,14 @@ function SearchDrop({prevSearch,handleLinkClick}) {
                  <div className=' grid-cols-2 grid md:grid-cols-3 gap-x-[1%] gap-y-[2%] w-full'>
                       {data.map((v,i)=>{
                             return(<button onClick={()=>handleLinkClick(v[0])} className='w-[100%] cursor-pointer group flex flex-row mt-[5%]  items-center' key={v+" "+i}>
-                                    <div className=' w-full md:w-[40%] cursor-pointer bg-hov group-hover:bg-hov2 bg-cover bg-center relative pb-[42%] rounded-l-xl rounded-r-xl md:rounded-r-[0]'>
+                                    <div className=' w-full pb-[60%] sm:w-[40%] cursor-pointer bg-hov group-hover:bg-hov2 bg-cover bg-center relative sm:pb-[42%] rounded-l-xl rounded-r-xl md:rounded-r-[0]'>
                                         <LazyLoadImage
-                                          className='absolute w-full h-full rounded-l-xl rounded-r-xl md:rounded-r-[0]'
+                                          className='absolute w-full h-full rounded-l-xl rounded-r-xl sm:rounded-r-[0]'
                                           alt={v[0]}
                                           src={v[1]} 
                                      />
                                     </div>
-                                    <label className='p-[2%] hidden cursor-pointer bg-hov group-hover:bg-hov2 w-[60%] rounded-r-xl h-full md:flex items-center' htmlFor="">{v[0]}</label>
+                                    <label className='p-[2%] hidden cursor-pointer bg-hov group-hover:bg-hov2 w-[60%] rounded-r-xl h-full sm:flex items-center' htmlFor="">{v[0]}</label>
                             </button>)
                       })}
                  </div>
