838bff346932c60c529b7cb7274fbec746bcc294
unknown <osamadrbas31@gmail.com>
2024-03-05 13:30:08 +0200

    finishing project

--- src/components/navBar/SearchDrop.jsx
+++ src/components/navBar/SearchDrop.jsx
@@ -15,9 +15,9 @@ function SearchDrop({prevSearch,handleLinkClick}) {
             <div className=' grid-cols-2 grid md:grid-cols-3 mb-[4%] sm:mb-[2%] gap-x-[1%] gap-y-[2%] w-full'>
                 {data.map((v,i)=>{
                       return(<button onClick={()=>handleLinkClick(v[0])} className='w-[100%] cursor-pointer group flex flex-row mt-[5%]  items-center' key={v+" "+i}>
-                              <div className=' w-full pb-[60%] sm:w-[40%] cursor-pointer bg-hov group-hover:bg-hov2 bg-cover bg-center relative sm:pb-[42%] rounded-l-xl rounded-r-xl md:rounded-r-[0]'>
+                              <div className=' w-full overflow-hidden pb-[60%] sm:w-[40%] cursor-pointer bg-hov group-hover:bg-hov2 bg-cover bg-center relative sm:pb-[42%] rounded-l-xl rounded-r-xl md:rounded-r-[0]'>
                                   <LazyLoadImage
-                                    className='absolute  h-full rounded-l-xl rounded-r-xl sm:rounded-r-[0]'
+                                    className='absolute w-full min-h-full rounded-l-xl rounded-r-xl sm:rounded-r-[0]'
                                     alt={v[0]}
                                     src={v[1]} 
                                   />
@@ -33,12 +33,13 @@ function SearchDrop({prevSearch,handleLinkClick}) {
             <div className=' grid-cols-2 grid md:grid-cols-3 mb-[2%] gap-x-[1%] gap-y-[2%] sm:pb-[0%] pb-[20%] w-full'>
                       {popular.map((v,i)=>{
                             return(<button onClick={()=>handleLinkClick(v.name)} className='w-[100%] cursor-pointer group flex flex-row mt-[5%]  items-center' key={v.name+" "+i}>
-                                    <div className=' w-full pb-[60%] sm:w-[40%] cursor-pointer bg-hov group-hover:bg-hov2 bg-cover bg-center relative sm:pb-[42%] rounded-l-xl rounded-r-xl md:rounded-r-[0]'>
+                                    <div className=' w-full pb-[60%] overflow-hidden sm:w-[40%] cursor-pointer bg-hov group-hover:bg-hov2 bg-cover bg-center relative sm:pb-[42%] rounded-l-xl rounded-r-xl md:rounded-r-[0]'>
                                         <LazyLoadImage
-                                          className='absolute  h-full rounded-l-xl rounded-r-xl sm:rounded-r-[0]'
+                                          className='absolute w-full h-full rounded-l-xl rounded-r-xl sm:rounded-r-[0]'
                                           alt={v.name}
                                           src={v.photo} 
                                         />
+                                        <label htmlFor=""></label>
                                     </div>
                                     <label className='p-[2%] hidden cursor-pointer bg-hov group-hover:bg-hov2 w-[60%] rounded-r-xl h-full sm:flex items-center' htmlFor="">{v.name}</label>
                             </button>)
